24d228c046457967688b9bc6dd40f5a384da2c6a
Shreyansh Khemesara <202001121@daiict.ac.in>
2021-01-31 14:18:40 +0530

    uploading my html css js studying files(made these files for practice

---
+++ html.js
@@ -0,0 +1,12 @@
+var number1 =80;
+var number2 =90;
+
+var str = 'this is a string';
+var marks = {
+    shreya = 90,
+    shreyansh =900,
+    monika =999,
+    sunil = 809809,
+}  
+
+console.log(marks);
